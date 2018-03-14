651fcd0f272c6f5419e421772ad02cf9c3b1e48b
Roselou <elizabethroselou@gmail.com>
2018-03-13 20:18:10 -0700

    practice with js

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,25 @@
-console.log("login.js loaded");
+var userLogin =
+{
+	userName: "roselou",
+	password: "Stella143"
+}
+
+var passwordInput; 
+
+var loggedIn = false; 
+
+let failures = 0;
+
+while (!loggedIn && failures < 3) {
+	passwordInput = prompt("Enter the password for user " + userLogin.userName + ".")
+	if (passwordInput === userLogin.password){
+		console.log("Correct password");
+		loggedIn = true;
+	} else {
+		console.log("Incorrect password. Try again.");''
+		failures++
+	}
+}
+if (failures === 3) {
+	alert("Wrong password entered too many times. You are now blocked.")
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,24 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+{ question: "What was your first pet's name?", 
+  expectedAnswer:"FlufferNutter"
+},
+{ 
+	question: "Which city did your parents meet?",
+	expectedAnswer: "Toronto"
+},
+{
+	question: "What is your mother's maiden name?"
+    expectedAnswer: "Pedersen"
+}
+]
+
+for (let i = 0; i < securityQuestions.length; i++) {
+	let userAnswer = prompt(securityQuestions[i].question);
+	if (userAnswer !== securityQuestions[i].expectedAnswer) {
+		alert("Incorrect response");
+		console.log("Stop!");
+		break;
+	}
+}
